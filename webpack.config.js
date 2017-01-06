eedd331cc7804b6a7279703edbc35edd855b5549
Martijn Senden <martijn@instantmagazine.com>
2017-01-06 18:32:49 +0100

    Was missing some files in the last commit!

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,23 @@
 const path = require('path')
-const webpack = require('webpack')
+require('webpack')
 
 module.exports = {
   context: __dirname,
-  entry: './js/ClientApp.js',
-  // devtool: 'cheap-module-source-map',
+  entry: './src/index.js',
   output: {
     path: path.join(__dirname, '/public'),
     filename: 'bundle.js',
-    publicPath: '/public/'
   },
   devServer: {
     publicPath: '/public/',
-    historyApiFallback: true
   },
   resolve: {
     extensions: ['.js', '.json'],
-    alias: {
-      react: 'preact-compat',
-      'react-dom': 'preact-compat'
-    }
   },
   stats: {
     colors: true,
     reasons: true,
-    chunks: true
+    chunks: true,
   },
   module: {
     rules: [
@@ -32,19 +25,18 @@ module.exports = {
         enforce: 'pre',
         test: /\.js$/,
         loader: 'eslint-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.json$/,
-        loader: 'json-loader'
+        loader: 'json-loader',
       },
       {
         include: [
-          path.resolve(__dirname, 'js'),
-          path.resolve('node_modules/preact-compat/src')
+          path.resolve(__dirname, 'src'),
         ],
         test: /\.js$/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.css$/,
@@ -53,11 +45,11 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              url: false
-            }
-          }
-        ]
-      }
-    ]
-  }
+              url: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
 }
